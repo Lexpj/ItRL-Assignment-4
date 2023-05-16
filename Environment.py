108f9355fa4fb4cec69da7f0c808981874d6508e
Lexpj <lex.janssens@live.nl>
2023-05-16 22:50:23 +0200

    Adjusted to discussed environment, removed treat

--- Environment.py
+++ Environment.py
@@ -8,105 +8,69 @@ import matplotlib as mpl
 class TomAndJerryEnvironment:
 
     class Cat:
-        def __init__(self, mid:tuple):
+        def __init__(self, coord:tuple):
             """
-            mid: coordinate
+            coord: coordinate
             """    
-            self.mid = np.array(mid)
-            self.time = np.random.randint(0,16) # Random number between 0 and 15
+            self.coord = np.array(coord)
+            self.maxtime = 16
+            self.time = np.random.randint(0,self.maxtime)
 
-            self.mirrorX = np.random.randint(0,1)
-            self.mirrorY = np.random.randint(0,1)
             self.speed = np.random.choice([-1,1],size=1)
             
-            self.eating = 0
-            
-            self.path = [
+            self.path = [ # gets overwritten anyways in setPath
                 [(0,),(1,11),(2,10)],
                 [(5,15),(4,12),(3,9)],
                 [(6,14),(7,13),(8,)]
             ]
-            self.transformPath()
-            
 
-        def transformPath(self):
-            """
-            Adjust the path by mirroring the self.path 2D array
-            Mirroring can happen in the X-axis and in the Y-axis
-            Along with this, the speed (-1 or 1) is the last randomizable factor,
-            allowing up to 2^3 = 8 variations of the path
-            """
-            if self.mirrorX:
-                self.path = self.path[::-1]
-            if self.mirrorY:
-                self.path = [x[::-1] for x in self.path]    
-
-        def step(self,treats):
+        def setPath(self,path):
+            self.path = path
+            self.maxtime = max([max([max(i) for i in x]) for x in path]) + 1
+            self.time = np.random.randint(0,self.maxtime)
+        
+        def step(self):
             """
             Adjust the timestep, and thus the position when called
             """
-            # Get position
-            pos = self.mid + self.pos()
-            toBeDeleted = []
-            
-            for treat in treats:
-                if np.all(pos == treat):
-                    toBeDeleted.append(treat)
-                    self.eating += 2
-            
-            for treat in toBeDeleted:
-                treats.remove(treat)
-            
-            if not self.eating:
-                self.time = (self.time+self.speed+16)%16
-            else:
-                self.eating -= 1
-            
-            return self.eating > 0
-
+            self.time = (self.time+self.speed+self.maxtime)%self.maxtime
 
         def pos(self):
             """
             Returns the delta position of the mid point of the cat
             """
             # find the current time in path
-            for i in range(3):
-                for j in range(3):
+            for i in range(len(self.path)):
+                for j in range(len(self.path[0])):
                     if self.time in self.path[i][j]:
-                        return np.array([j-1,i-1])
+                        return np.array([j,i])
             raise Exception("Cat made invalid move") # shouldn't occur
 
     def __init__(self, render_mode = None):        
         self.env = [
-            '0000000',
-            '0000011',
-            '0000000',
-            '0001000',
-            '0001000',
-            '0001001',
-            '0001000',
-            '0000000'
+            '00000',
+            '00100',
+            '00100',
+            '00100',
+            '00000'
         ]
 
         self.rewards = {
             'cat': -10,
             'step': -1,
             'goal': 30,
-            'eating': 50
         }
 
-        self.width = 7
-        self.height = 8
+        self.width = 5
+        self.height = 5
 
         self.n_states = self.height * self.width
-        self.n_actions = 5
+        self.n_actions = 4
                 
         self.cats = []
-        self.goal = np.array((5,6))
+        self.goal = np.array((4,3))
         self.done = False
         self.state = self.reset()
-        self.nr_treats = 1
-        self.treats = []
         
         if render_mode == "human":
             self._render_setup()
@@ -130,16 +94,23 @@ class TomAndJerryEnvironment:
         np.random.seed(seed)
 
         self.done = False
-        self.nr_treats = 1
         self.treats = []
         
         self.cats = [
-            self.Cat((1,5)),
-            self.Cat((5,3)),
-            self.Cat((5,6))
+            self.Cat((0,2)),
+            self.Cat((3,0))
         ]
-
-        s = self.posToState((0,1))
+        self.cats[0].setPath([
+            [(0,),(1,)],
+            [(5,),(2,)],
+            [(4,),(3,)]
+        ])
+        self.cats[1].setPath([
+            [(0,),(1,)],
+            [(3,),(2,)],
+        ])
+        
+        s = self.posToState((0,0))
         self.state = s
         return s
 
@@ -153,21 +124,17 @@ class TomAndJerryEnvironment:
         """
         Returns the step definitions by action.
         """
-        return {0:np.array((0,-1)),1:np.array((-1,0)),2:np.array((0,1)),3:np.array((1,0)),4:np.array((0,0))}
+        return {0:np.array((0,-1)),1:np.array((-1,0)),2:np.array((0,1)),3:np.array((1,0))}
        
     def step(self,a):
         ''' Forward the environment based on action a 
         Returns the next state, the obtained reward, and a boolean whether the environment terminated '''
         
         r = 0
-        # If treats available, lay one down
-        if a == 4 and self.nr_treats > 0:
-            self.nr_treats -= 1
-            self.treats.append(self.stateToPos(self.state))
         
         # info
-        info = {'treatsLeft':self.treats}
-
+        info = {}
+        
         # Move the agent
         s_next = self.stateToPos(self.state) + self._get_action_definitions()[a] 
         # bound within grid
@@ -176,21 +143,25 @@ class TomAndJerryEnvironment:
 
         # if cat is hit
         for cat in self.cats:
-            mid = cat.mid
-            eating = cat.step(self.treats)
-            if eating: r += self.rewards['eating'] 
+            coord = cat.coord
+            oldpos = cat.pos()
+            
+            cat.step()
 
             delta = cat.pos()
-            pos = mid+delta
+            pos = coord+delta
+            
             if np.all(s_next == pos):
                 self.done = True
                 r += self.rewards['cat']
                 return s_next, r, self.done, info
+            elif np.all(oldpos == s_next) and np.all(self.stateToPos(self.state) == delta):
+                self.done = True
+                r += self.rewards['cat']
+                return s_next, r, self.done, info
 
-        # check if wall is hit
-        if self.env[s_next[1]][s_next[0]] == '1':
-            pass # no update
-        else:
+        # check if not wall is hit
+        if self.env[s_next[1]][s_next[0]] != '1':
             self.state = self.posToState(s_next)
 
         # Check reward and termination
@@ -213,9 +184,9 @@ class TomAndJerryEnvironment:
             curMap[treat[1]][treat[0]] = "T"
             
         for cat in self.cats:
-            mid = cat.mid
+            coord = cat.coord
             delta = cat.pos()
-            pos = mid+delta
+            pos = coord+delta
 
             curMap[pos[1]][pos[0]] = 'C'
         
@@ -282,7 +253,7 @@ def test():
     # Test
     while not done:
         #a = int(input())
-        a = np.random.randint(5) # sample random action    
+        a = np.random.randint(4) # sample random action    
         s_next,r,done,info = env.step(a) # execute action in the environment
         #env.render(Q_sa=Q_sa,plot_optimal_policy=False,step_pause=step_pause) # display the environment
         env.render()
